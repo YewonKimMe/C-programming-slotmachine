fc2f41976a8038aa1df51a25447b164380ff3fc7
YewonKimMe <kimyewon030428@gmial.com>
2024-05-01 01:46:53 +0900

    함수 주석 수정

--- SlotMachine/get_random_value.c
+++ SlotMachine/get_random_value.c
@@ -6,9 +6,18 @@
 #include "get_random_value.h"
 #include "moneyUnitConvertor.h"
 
+/*
+	파일명:	get_random_value.c
+	설명: 자연수 x, 0<x<1 사이의 double type의 확률 변수를 반환하는 함수
+	종류: Function
+		@Param	: void
+		@Return	: double
+	수정일: 2024-05-01
+*/
 double get_random_value(void) 
 {
-	srand(time(NULL));
+	srand((unsigned)time(NULL));
 	double random_num = (double)rand() / (RAND_MAX + 1.0);
+	printf("[DEBUG]생성 랜덤값: %.6f\n", random_num);
 	return random_num;
 }
--- SlotMachine/get_start_mode.c
+++ SlotMachine/get_start_mode.c
@@ -6,7 +6,15 @@
 #include <stdio.h>
 #include "get_start_mode.h"
 
-int get_start_mode()
+/*
+	파일명:	get_start_mode.c
+	설명: 게임 시작 모드를 입력받고 처리하는 함수
+	종류: Function
+		@Param	: void
+		@Return	: 1(int), 0(int), 9999(int)
+	수정일: 2024-05-01
+*/
+int get_start_mode(void)
 {
 	int game_mode;
 	while (1) // 특정 케이스를 입력 받을 동안 무한 반복
@@ -17,7 +25,7 @@ int get_start_mode()
 			return START_GAME;
 		else if (game_mode == END_GAME) // 게임 종료
 			return END_GAME;
-		else if (game_mode == TEST_MODE) // 테스트 모드-9번
+		else if (game_mode == TEST_MODE) // 테스트 모드 - 9999 입력
 			return TEST_MODE;
 		else
 		{
