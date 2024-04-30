0214d9b5b0f4a4bdf74e949a5410ed86d02a40f0
YewonKimMe <kimyewon030428@gmial.com>
2024-05-01 03:33:59 +0900

    메인함수 작업내용 임시 커밋

--- SlotMachine/slotmachine.c
+++ SlotMachine/slotmachine.c
@@ -1,28 +1,65 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
+#define START_GAME 1
+#define END_GAME 0
+#define TEST_MODE 9
 
 #include <stdio.h>
 #include <stdlib.h>
+#include<time.h>
 #include "betting.h"
 #include "moneyUnitConvertor.h"
+#include "get_start_mode.h"
+#include "get_random_value.h"
+#include "handle_random_event.h"
+#include "percentage_test.h"
 
 #define MAX_PATTERN 7
 #define START_MONEY 10000000
+#define JACKPOT_MULTIPLE 20
 
 int main(void)
-{
+{   
+    int game_flag = 1;
     int money = START_MONEY;
-    const char* convertedMoeny1 = moneyUnitConvertor(&money);
-    printf("초기 보유 금액은 %s 원 입니다.\n", convertedMoeny1);
-    free(convertedMoeny1);
 
-    int betMoney = betting(&money);
-    const char* convertedMoeny3 = moneyUnitConvertor(&betMoney);
-    printf("배팅 금액은 %s 원 입니다.\n", convertedMoeny3);
-    free(convertedMoeny3);
+    while (game_flag && money > 0)
+    {
+        int game_mode = get_start_mode(); // 사용자로부터 게임 시작 모드 획득
+        if (game_mode == START_GAME) // 게임 시작
+        {
+            printf("게임을 시작합니다.\n");
+            const char* convertedMoeny1 = moneyUnitConvertor(&money);
+            printf("초기 보유 금액은 %s 원 입니다.\n", convertedMoeny1);
+            free(convertedMoeny1);
 
-    char* convertedMoeny2 = moneyUnitConvertor(&money);
-    printf("배팅 후 잔액은 %s 원 입니다.\n", convertedMoeny2);
-    free(convertedMoeny2);
+            int betMoney = betting(&money);
+            const char* convertedMoeny3 = moneyUnitConvertor(&betMoney);
+            printf("배팅 금액은 %s 원 입니다.\n", convertedMoeny3);
+            free(convertedMoeny3);
+
+            char* convertedMoeny2 = moneyUnitConvertor(&money);
+            printf("배팅 후 잔액은 %s 원 입니다.\n", convertedMoeny2);
+            free(convertedMoeny2);
+
+            
+            double probability = get_random_value(); // 추첨
+            int k = handle_random_event(probability, &money);
+
+        }
+        else if (game_mode == END_GAME) // 게임 종료
+        {
+            printf("사용자의 요청에 따라 게임을 종료합니다.\n");
+            return 0;
+        }
+        else // 테스트 모드_개발자용
+        {
+            printf("테스트 모드로 진입합니다..\n");
+            percentage_test();
+            break;
+        }
+
+    }
+
     return 0;
 }
 /*
