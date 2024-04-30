e60799688898a2cbb1d2a391c8445a4d44230873
YewonKimMe <kimyewon030428@gmial.com>
2024-05-01 01:47:25 +0900

    handle_random_event 추가

---
+++ SlotMachine/handle_random_event.c
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define BOUNDARY_JACKPOT 0.001
+#define BOUNDARY_THREE_MATCH 0.01
+#define BOUNDARY_TWO_MATCH 0.05
+#define BOUNDARY_ONE_MATCH 0.2
+
+#include <stdio.h>
+#include "handle_random_event.h"
+
+/*
+	파일명:	handle_random_event.c
+	설명: 확률변수값을 인자로 받아 변수 경계값 별로 추첨 처리 및 상금 가감을 하는 함수
+	종류: Function
+		@Param	: proability(double), money(int*)
+		@Return	: int
+	수정일: 2024-05-01
+*/
+int handle_random_event(double probability, int* money)
+{
+	printf("[arg 확률값]: %.10f, [PERCENTAGE]: %.2f%%\n", probability, probability * 100);
+	if (0 < probability && probability < BOUNDARY_JACKPOT) { // 0 < x < jackpot
+		printf("잭팟 당첨, 확률값: %.5f\n", probability);
+	}
+	else if (BOUNDARY_JACKPOT <= probability && probability < BOUNDARY_THREE_MATCH) { // jackpot <= x < three_match
+		printf("3개 일치, 확률값: %.5f\n", probability);
+	}
+	else if (BOUNDARY_THREE_MATCH <= probability && probability < BOUNDARY_TWO_MATCH) {
+		printf("2개 일치, 확률값: %.5f\n", probability);
+	}
+	else if (BOUNDARY_TWO_MATCH <= probability && probability < BOUNDARY_ONE_MATCH) {
+		printf("1개 일치, 확률값: %.5f\n", probability);
+	}
+	else {
+		printf("0개 일치, 확률값: %.5f\n", probability);
+	}
+	return 0;
+}
---
+++ SlotMachine/handle_random_event.h
@@ -0,0 +1,7 @@
+#ifndef HANDLE_RANDOM_EVENT_H
+
+#define HANDLE_RANDOM_EVENT_H
+
+int handle_random_event(double probability, int* money);
+
+#endif
