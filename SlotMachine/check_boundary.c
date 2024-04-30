281145135600238db552f0ba42768eeb87487306
YewonKimMe <kimyewon030428@gmial.com>
2024-05-01 03:29:49 +0900

    확률 변경 및 경계조건 수정

--- SlotMachine/check_boundary.c
+++ SlotMachine/check_boundary.c
@@ -2,7 +2,8 @@
 #define BOUNDARY_JACKPOT 0.001
 #define BOUNDARY_THREE_MATCH 0.01
 #define BOUNDARY_TWO_MATCH 0.05
-#define BOUNDARY_ONE_MATCH 0.2
+#define BOUNDARY_ONE_MATCH 0.3
+
 #define CODE_JACKPOT 0
 #define CODE_THREE_MATCH 1
 #define CODE_TWO_MATCH 2
@@ -12,21 +13,29 @@
 #include <stdio.h>
 #include "check_boundary.h"
 
-int boundary_check(double probability)
+/*
+	파일명:	check_boundary.c
+	설명: 랜덤값을 받아 경계값에 따른 조건 처리, 확률 관련 함수(확률 경계값이 이곳에 선언되어 있음)
+	종류: Function
+		@Param	: probability(double)
+		@Return	: 0(int) or 1(int) or 2(int) or 3(int) or 4(int) 
+	수정일: 2024-05-01
+*/
+int check_boundary(double probability)
 {
-	if (0 < probability && probability < BOUNDARY_JACKPOT) { // 0 < x < jackpot
+	if (0 < probability && probability <= BOUNDARY_JACKPOT) { // 0 < x < jackpot
 		return CODE_JACKPOT;
 	}
-	else if (BOUNDARY_JACKPOT <= probability && probability < BOUNDARY_THREE_MATCH) { // jackpot <= x < three_match
+	else if (BOUNDARY_JACKPOT < probability && probability <= BOUNDARY_THREE_MATCH) { // jackpot <= x < three_match
 		return CODE_THREE_MATCH;
 	}
-	else if (BOUNDARY_THREE_MATCH <= probability && probability < BOUNDARY_TWO_MATCH) {
+	else if (BOUNDARY_THREE_MATCH < probability && probability <= BOUNDARY_TWO_MATCH) {
 		return CODE_TWO_MATCH;
 	}
-	else if (BOUNDARY_TWO_MATCH <= probability && probability < BOUNDARY_ONE_MATCH) {
+	else if (BOUNDARY_TWO_MATCH < probability && probability <= BOUNDARY_ONE_MATCH) {
 		return CODE_ONE_MATCH;
 	}
-	else {
+	else if (BOUNDARY_ONE_MATCH < probability && probability <= 1){
 		return CODE_NO_MATCH;
 	}
 }
