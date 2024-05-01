247cf9e69bdd56e9114d053121e754bda2d30306
YewonKimMe <kimyewon030428@gmial.com>
2024-05-01 23:27:24 +0900

    테스트 출력 메세지 포맷 추가 print_test_message_num, print_test_message_str, print_test_result_message, print_percent_test_result, print_test_success_message, print_test_fail_message, print_test_time

--- SlotMachine/print_test_message.c
+++ SlotMachine/print_test_message.c
@@ -1,13 +1,46 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define GREEN "\033[32m"
+#define RED "\033[31m"
+#define RESET "\033[0m"
 
 #include <stdio.h>
 
 void print_test_message(char* message)
 {
-	printf("[TEST]\t%s\n", message);
+	printf("[TEST]\t\t%s\n", message);
 }
 
-void print_test_result_format(char* message, int cnt, int attempt_cnt, double percentage)
+void print_test_message_num(char* message, int num)
 {
-	printf("[TEST_RESULT] %s,\t등장 횟수={%10d},\t시도 횟수={%d},\t확률 테스트 결과={%.4f %%}\n", message, cnt, attempt_cnt, percentage);
+	printf("[TEST]\t\t%s = %d\n", message, num);
+}
+
+void print_test_message_str(char* message, char* str)
+{
+	printf("[TEST]\t\t%s = %s\n", message, str);
+}
+
+void print_test_result_message(char* message)
+{
+	printf("[TEST_RESULT]\t%s\n", message);
+}
+
+void print_test_success_message(char* message)
+{
+	printf("%s[TEST_SUCCESS]\t%s%s\n", GREEN, message, RESET);
+}
+
+void print_test_fail_message(char* message)
+{
+	printf("%s[TEST_FAIL]\t%s%s\n", RED, message, RESET);
+}
+
+void print_percent_test_result(char* message, int cnt, int attempt_cnt, double percentage)
+{
+	printf("[TEST_RESULT]\t%s,\t등장 횟수={%10d},\t시도 횟수={%d},\t확률 테스트 결과={%-5.2f %%}\n", message, cnt, attempt_cnt, percentage);
+}
+
+void print_test_time(double used_time)
+{
+	printf("[TEST_TIME]\t테스트 수행시간: %.3f (초)\n", used_time);
 }
--- SlotMachine/print_test_message.h
+++ SlotMachine/print_test_message.h
@@ -4,6 +4,18 @@
 
 void print_test_message(char* message);
 
-void print_test_result_format(char* message, int cnt, int attempt_cnt, double percentage);
+void print_test_message_num(char* message, int num);
+
+void print_test_message_str(char* message, char* str);
+
+void print_test_result_message(char* message);
+
+void print_percent_test_result(char* message, int cnt, int attempt_cnt, double percentage);
+
+void print_test_success_message(char* message);
+
+void print_test_fail_message(char* message);
+
+void print_test_time(double used_time);
 
 #endif
