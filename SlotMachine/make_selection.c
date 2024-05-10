dc9cd6135d607c637536176b6425ee0721ce0448
YewonKimMe <kimyewon030428@gmial.com>
2024-05-11 02:45:53 +0900

    make_selection: 확률에 따라 결과값 조절 함수 추가

---
+++ SlotMachine/make_selection.c
@@ -0,0 +1,8 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdint.h>
+
+int make_selection(double probability, int user_selected[3], int to_be_made[3])
+{
+	return 1;
+}
---
+++ SlotMachine/make_selection.h
@@ -0,0 +1,5 @@
+#ifndef MAKE_SELECTION_H
+
+#define MAKE_SELECTION_H
+int make_selection(double probability, int user_selected[3], int to_be_made[3]);
+#endif
