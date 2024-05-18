e6846b0c09450f55825a8e3748b9f7d7130c6152
YewonKimMe <kimyewon030428@gmial.com>
2024-05-19 07:10:33 +0900

    출력 모습 변경

--- SlotMachine/print_selection_raffle_result.c
+++ SlotMachine/print_selection_raffle_result.c
@@ -7,21 +7,23 @@ extern const char* yellow;
 extern const char* reset;
 void print_selection_raffle_result(int user_bat[3], int prob_random_selected[3])
 {
-	printf("유저 배팅 숫자: [ ");
+	printf("\t\t===================================\n");
+	printf("유저 배팅 숫자: ");
 	for (int i = 0; i < 3; i++) {
 		if (user_bat[i] == prob_random_selected[i])
-			printf("%s%d%s ", yellow, user_bat[i], reset);
+			printf("| >> %s%d%s << | ", yellow, user_bat[i], reset);
 		else
-			printf("%d ", user_bat[i]);
+			printf("| >> %d << | ", user_bat[i]);
 	}
-	printf("]\n");
+	printf("\n");
 
-	printf("추첨 숫자 결과: [ ");
+	printf("추첨 숫자 결과: ");
 	for (int i = 0; i < 3; i++) {
 		if (user_bat[i] == prob_random_selected[i])
-			printf("%s%d%s ", yellow, prob_random_selected[i], reset);
+			printf("| >> %s%d%s << | ", yellow, prob_random_selected[i], reset);
 		else
-			printf("%d ", prob_random_selected[i]);
+			printf("| >> %d << | ", prob_random_selected[i]);
 	}
-	printf("]\n");
+	printf("\n");
+	printf("\t\t===================================\n");
 }
