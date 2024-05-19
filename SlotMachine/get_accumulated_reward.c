e9f7f71494e51bba77f51363de28a8915c44a7b3
YewonKimMe <kimyewon030428@gmial.com>
2024-05-19 17:37:12 +0900

    안내문구 변경

--- SlotMachine/get_accumulated_reward.c
+++ SlotMachine/get_accumulated_reward.c
@@ -7,12 +7,12 @@ int get_accumulated_reward(const char* file_name) {
     int accumulated_money = 0;
     FILE* fp = fopen(file_name, "r");
     if (fp == NULL) {
-        fprintf(stderr, "파일 %s를 열 수 없습니다.\n", file_name);
+        fprintf(stderr, "파일 %s를 열 수 없습니다.\nSlotMachine/SlotMachine 디렉토리 내에 accm_reward.txt 파일이 존재하는지 확인해 주세요.\n", file_name);
         exit(1);
     }
 
     if (fscanf(fp, "%d", &accumulated_money) != 1) {
-        fprintf(stderr, "파일 %s에서 정수를 읽을 수 없습니다.\n", file_name);
+        fprintf(stderr, "파일 %s에서 정수(누적금액) 를 읽을 수 없습니다.\nSlotMachine/SlotMachine 디렉토리 내에 accm_reward.txt의 첫번째 줄에 정수가 존재하는지 확인해 주세요.\n", file_name);
         fclose(fp);
         exit(1);
     }
