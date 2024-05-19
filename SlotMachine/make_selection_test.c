9dfc7b532c8b7b6d6b8f90e2df04a368e7db2d06
YewonKimMe <kimyewon030428@gmial.com>
2024-05-19 11:51:30 +0900

    색상코드 및 print_two_array 추가

--- SlotMachine/make_selection_test.c
+++ SlotMachine/make_selection_test.c
@@ -9,7 +9,36 @@ extern const int code_three_match; // 1
 extern const int code_two_match; // 2
 extern const int code_one_match; // 3
 extern const int code_no_match; // 4
+extern const char* green;
+extern const char* yellow;
+extern const char* reset;
 
+/*
+	두 개의 배열을 입력받아 일치 여부를 확인하여 출력
+*/
+void print_two_array(int array1[3], int array2[3], char* message) {
+	print_test_result_message(message);
+	print_test_result_message("유저 입력값");
+	for (int i = 0; i < 3; i++) {
+		if (array1[i] == array2[i])
+			printf("%s%d%s ", green, array1[i], reset);
+		else
+			printf("%d ", array1[i]);
+	}
+	printf("\n");
+	print_test_result_message("추첨 생성값");
+	for (int i = 0; i < 3; i++) {
+		if (array1[i] == array2[i])
+			printf("%s%d%s ", green, array2[i], reset);
+		else
+			printf("%d ", array2[i]);
+	}
+	printf("\n\n");
+}
+
+/*
+	조건 code에 따라 배열 일치 여부 및 잭팟 여부를 검증
+*/
 int check_array_make(int array1[3], int array2[3], int code, int* count_7) {
 	make_selection(code, array1, array2); // 결과 코드, 유저 입력 배열, 생성될 배열
 	int count = 0;
@@ -22,39 +51,49 @@ int check_array_make(int array1[3], int array2[3], int code, int* count_7) {
 	}
 	return count;
 }
-
+/*
+	파일명:	make_selection_test.c
+	설명: 유저가 입력한 값 3개와 추첨으로 생성된 값 3개가 확률 코드에 따라 일치하는지 확인하는 테스트 코드
+	종류: Function
+	수정일: 2024-05-19
+*/
 void make_selection_test(void)
 {
 	int user_input[3] = { 3, 4, 1 }; // 유저가 입력한 값, 0~5 아무거나.
 	int to_be_made[3] = { 0, 0, 0 }; // 생성될 배열
 	int count_7 = 0;
 
-	print_test_message("추첨 숫자 생성/일치 테스트");
+	print_test_message("추첨 숫자 생성/일치 테스트_순서 고려(default)");
 
 	check_array_make(user_input, to_be_made, code_jackpot, &count_7); // 잭팟 검증
-	printf("count7=%d\n", count_7);
+
 	if (count_7 == 3)
 		print_test_success_message("잭팟 추첨 숫자 검증 성공");
 	else
 		print_test_fail_message_default("잭팟 추첨 숫자 검증 실패");
+	print_two_array(user_input, to_be_made, "잭팟 테스트 결과"); // 잭팟 테스트 케이스 출력
 
-	if (check_array_make(user_input, to_be_made, code_three_match, &count_7) == 3)
+	if (check_array_make(user_input, to_be_made, code_three_match, &count_7) == 3) // 3개 일치 검증
 		print_test_success_message("3개 숫자 일치 검증 성공");
 	else
 		print_test_fail_message_default("3개 숫자 일치 검증 실패");
+	print_two_array(user_input, to_be_made, "3개 일치 테스트 결과"); // 3개 일치 테스트 케이스 출력
 
-	if (check_array_make(user_input, to_be_made, code_two_match, &count_7) == 2)
+	if (check_array_make(user_input, to_be_made, code_two_match, &count_7) == 2) // 2개 일치 검증
 		print_test_success_message("2개 숫자 일치 검증 성공");
 	else
 		print_test_fail_message_default("2개 숫자 일치 검증 실패");
+	print_two_array(user_input, to_be_made, "2개 일치 테스트 결과"); // 2개 일치 테스트 케이스 출력
 
-	if (check_array_make(user_input, to_be_made, code_one_match, &count_7) == 1)
+	if (check_array_make(user_input, to_be_made, code_one_match, &count_7) == 1) // 1개 일치 검증
 		print_test_success_message("1개 숫자 일치 검증 성공");
 	else
 		print_test_fail_message_default("1개 숫자 일치 검증 실패");
+	print_two_array(user_input, to_be_made, "1개 일치 테스트 결과"); // 1개 일치 테스트 케이스 출력
 
-	if (check_array_make(user_input, to_be_made, code_no_match, &count_7) == 0)
+	if (check_array_make(user_input, to_be_made, code_no_match, &count_7) == 0) // 0개 일치 검증
 		print_test_success_message("0개 숫자 일치 검증 성공");
 	else
 		print_test_fail_message_default("0개 숫자 일치 검증 실패");
+	print_two_array(user_input, to_be_made, "0개 일치 테스트 결과"); // 0개 일치 테스트 케이스 출력
 }
--- SlotMachine/make_selection_test.h
+++ SlotMachine/make_selection_test.h
@@ -1,5 +1,9 @@
 #ifndef MAKE_SELECTION_TEST_H
 
 #define MAKE_SELECTION_TEST_H
+
+void print_two_array(int array1[3], int array2[3], char* message);
+
 void make_selection_test(void);
+
 #endif
