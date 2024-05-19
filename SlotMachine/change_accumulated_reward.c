c56ad6ad9ebe58e095919e8fb8bab0fb36691e5a
YewonKimMe <kimyewon030428@gmial.com>
2024-05-19 17:36:50 +0900

    change_accumulated_reward

---
+++ SlotMachine/change_accumulated_reward.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+// 누적 금액을 변경하는 함수, 기존 파일을 삭제하고 갱신된 누적금액을 다시 작성
+int change_accumulated_reward(int exist_acc_money, int use_money, char* file_name)
+{
+	int new_accumulated_reward = exist_acc_money + use_money;
+	FILE* fp = NULL;
+
+	if (remove(file_name) == -1)
+		printf("%s 를 삭제할 수 없습니다.\n", file_name);
+
+	fp = fopen(file_name, "a");
+
+	// 새로운 누적 금액 작성
+	fprintf(fp, "%d\n", new_accumulated_reward);
+
+	fclose(fp);
+
+	return new_accumulated_reward; // 새로운 누적 금액 return;
+	
+	
+}
---
+++ SlotMachine/change_accumulated_reward.h
@@ -0,0 +1,5 @@
+#ifndef CHANGE_ACCUMULATED_REWARD_H
+
+#define CHANGE_ACCUMULATED_REWARD_H
+int change_accumulated_reward(int exist_acc_money, int use_money, char* file_name);
+#endif
